e538c573fd9992a52ae3a8881eef6a20aed26cb2
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2014-08-22 17:27:18 -0400

    add build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+base=classes/cs422/2014fa
+full=http://zoo.cs.yale.edu/$base
+rm -rf compiled
+dir=$(dirname $(readlink -f $0))/compiled
+cp config.yml config.yml.bak
+sed "s#BASE_PATH#$base#g" -i config.yml
+sed "s#PRODUCTION_URL#$full#g" -i config.yml
+bundle exec ruhoh compile
+mv config.yml.bak config.yml
