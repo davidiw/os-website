1b639e7d1ea5b25e3b95e900dccfd07d028156cb
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2014-08-28 11:24:23 -0400

    hopefully the last of the build fixes

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 base=classes/cs422/2014fa
 full=http://zoo.cs.yale.edu/$base
-rm -rf compiled
 dir=$(dirname $(readlink -f $0))/compiled
 cp config.yml config.yml.bak
 sed "s#BASE_PATH#$base#g" -i config.yml
 sed "s#PRODUCTION_URL#$full#g" -i config.yml
-bundle exec ruhoh compile
+bundle exec ruhoh compile ../web.tmp
 mv config.yml.bak config.yml
-mv -f live old
-mv compiled/$base live
-rm -rf old compiled
+rm -rf ../web.live
+mv -f ../web.tmp ../web.live
 chmod g+w -R $PWD 2> /dev/null
